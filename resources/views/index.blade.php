df1ecb73b3bd806ca7e7a605a3a33424569f9f1a
Tiefing Sangaré <tiefingsangare86@gmail.com>
2026-04-18 13:21:13 +0000

    implement index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,197 +7,211 @@
 
 @section('content')
 <section class="hero" data-background="{{ asset('assets/img/hero/hero-bg.png') }}">
-            <div class="container">
-              <div class="hero__inner">
-                <div class="hero__content">
-                  <h1 class="hero-title">
-                    cruv
-                    <span class="des">
-                      Cruv digital agency committed <br>
-                      to enhancing user experiences by leveraging the
-                      advancements in technology ensuring innovative and
-                      impactful solutions.
-                    </span>
-                    <br>
+  <div class="container">
+    <div class="hero__inner">
+      <div class="hero__content">
+        <h1 class="hero-title">
+          Le branding ne commence pas par un logo.
+          <span class="des">
+            Nous aidons les entreprises et institutions à structurer leur marque et leur communication
+            afin de transformer leur image en levier de crédibilité, de fidélisation et de ventes durables.
+          </span>
+          <br>
 
-                    <img
-                      class="arrow"
-                      src="{{ asset('assets/img/hero/hero-arrow.png') }}"
-                      alt="image"
-                   >
-                    creative <br>
-                    design studi
-                    <img
-                      class="circle"
-                      src="{{ asset('assets/img/hero/hero-shape.png') }}"
-                      alt="image"
-                   >
-                  </h1>
-                  <div class="hero__featured">
-                    <div class="hero__featured__wrapper">
-                      <div class="swiper hero__featured__active">
-                        <div class="swiper-wrapper">
-                          <div class="swiper-slide">
-                            <div class="hero__featured__item">
-                              <img src="{{ asset('assets/img/hero/hero.png') }}" alt="image">
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
+          <img
+            class="arrow"
+            src="{{ asset('assets/img/hero/hero-arrow.png') }}"
+            alt="image"
+         >
 
-                    <div class="hero__featured__bottom">
-                      <h6 class="title">Featured Projects</h6>
-                      <div class="hero__featured__arrow">
-                        <div class="hero__featured__prev">
-                          <svg
-                            width="24"
-                            height="24"
-                            viewBox="0 0 24 24"
-                            fill="none"
-                            xmlns="http://www.w3.org/2000/svg"
-                          >
-                            <path
-                              d="M8 5C8 5.742 7.267 6.85 6.525 7.78C5.571 8.98 4.431 10.027 3.124 10.826C2.144 11.425 0.956 12 0 12M0 12C0.956 12 2.145 12.575 3.124 13.174C4.431 13.974 5.571 15.021 6.525 16.219C7.267 17.15 8 18.26 8 19M0 12H24"
-                              stroke="black"
-                              stroke-width="1.5"
-                            ></path>
-                          </svg>
-                        </div>
-                        <div class="hero__featured__next">
-                          <svg
-                            width="24"
-                            height="14"
-                            viewBox="0 0 24 14"
-                            fill="none"
-                            xmlns="http://www.w3.org/2000/svg"
-                          >
-                            <path
-                              d="M16 0C16 0.742 16.733 1.85 17.475 2.78C18.429 3.98 19.569 5.027 20.876 5.826C21.856 6.425 23.044 7 24 7M24 7C23.044 7 21.855 7.575 20.876 8.174C19.569 8.974 18.429 10.021 17.475 11.219C16.733 12.15 16 13.26 16 14M24 7H0"
-                              stroke="black"
-                              stroke-width="1.5"
-                           >
-                          </svg>
-                        </div>
-                      </div>
-                    </div>
+          Il commence par une décision stratégique.
+          <br>
+
+          Beaucoup d’entreprises communiquent.<br>
+          Peu ont une marque réellement structurée pour vendre et durer.
+
+          <img
+            class="circle"
+            src="{{ asset('assets/img/hero/hero-shape.png') }}"
+            alt="image"
+         >
+        </h1>
+
+        <div class="hero__featured">
+          <div class="hero__featured__wrapper">
+            <div class="swiper hero__featured__active">
+              <div class="swiper-wrapper">
+                <div class="swiper-slide">
+                  <div class="hero__featured__item">
+                    <img src="{{ asset('assets/img/hero/hero.png') }}" alt="image">
                   </div>
                 </div>
               </div>
             </div>
-          </section>
+          </div>
+
+          <div class="hero__featured__bottom">
+            <h6 class="title">Projets réalisés</h6>
+            <div class="hero__featured__arrow">
+              <div class="hero__featured__prev">
+                <svg
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                >
+                  <path
+                    d="M8 5C8 5.742 7.267 6.85 6.525 7.78C5.571 8.98 4.431 10.027 3.124 10.826C2.144 11.425 0.956 12 0 12M0 12C0.956 12 2.145 12.575 3.124 13.174C4.431 13.974 5.571 15.021 6.525 16.219C7.267 17.15 8 18.26 8 19M0 12H24"
+                    stroke="black"
+                    stroke-width="1.5"
+                  ></path>
+                </svg>
+              </div>
+
+              <div class="hero__featured__next">
+                <svg
+                  width="24"
+                  height="14"
+                  viewBox="0 0 24 14"
+                  fill="none"
+                >
+                  <path
+                    d="M16 0C16 0.742 16.733 1.85 17.475 2.78C18.429 3.98 19.569 5.027 20.876 5.826C21.856 6.425 23.044 7 24 7M24 7C23.044 7 21.855 7.575 20.876 8.174C19.569 8.974 18.429 10.021 17.475 11.219C16.733 12.15 16 13.26 16 14M24 7H0"
+                    stroke="black"
+                    stroke-width="1.5"
+                  >
+                </svg>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
           <!-- hero area start  -->
 
-          <section class="hero-video">
-            <div class="hero-video__wrapper">
-              <div class="work">
-                <h6><a href="#">EXPLORE WORKS</a></h6>
-              </div>
-              <div class="hero__play">
-                <a
-                  class="popup-video"
-                  href="https://www.youtube.com/watch?v=8oON21G1Bqg"
-                >
-                  <svg
-                    width="18"
-                    height="22"
-                    viewBox="0 0 18 22"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path d="M18 11L0 0.607697V21.3923L18 11Z" fill="black">
-                  </svg>
-                </a>
-
-                <a
-                  class="popup-video has--play"
-                  href="https://www.youtube.com/watch?v=8oON21G1Bqg"
-                >
-                  <span>PLAY REEL </span>
-                </a>
-              </div>
-              <div class="play">
-                <h6><a href="#about">SCROLL DOWN</a></h6>
-              </div>
+    <section class="hero-video">
+        <div class="hero-video__wrapper">
+            <div class="work">
+            <h6><a href="#">NOTRE EXPERTISE</a></h6>
             </div>
-          </section>
+
+            <div class="hero__play">
+            <a
+                class="popup-video"
+                href="https://www.youtube.com/watch?v=8oON21G1Bqg"
+            >
+                <svg
+                width="18"
+                height="22"
+                viewBox="0 0 18 22"
+                fill="none"
+                >
+                <path d="M18 11L0 0.607697V21.3923L18 11Z" fill="black">
+                </svg>
+            </a>
+
+            <a
+                class="popup-video has--play"
+                href="https://www.youtube.com/watch?v=8oON21G1Bqg"
+            >
+                <span>VOIR LA PRÉSENTATION</span>
+            </a>
+            </div>
+
+            <div class="play">
+            <h6><a href="#about">DÉCOUVRIR</a></h6>
+            </div>
+        </div>
+    </section>
 
           <section class="about-us theme-bg-gray section-space-top">
-            <div class="container">
-              <div class="about-us__inner">
-                <h6
-                  class="about-us__sub-title wow fade-in-left"
-                  data-wow-delay="600ms"
-                >
-                  <span>About Studio</span>
-                </h6>
-                <div
-                  class="about-us__content wow fade-in-bottom"
-                  data-wow-delay="600ms"
-                >
-                  <p>
-                    We are a creative studio that loves making amazing websites
-                    & cool products. We're really good at creating brands,
-                    designing cool stuff, and making things work just right. Our
-                    approach is all about understanding your needs to bringing
-                    ideas to life
-                  </p>
+  <div class="container">
+    <div class="about-us__inner">
+      <h6
+        class="about-us__sub-title wow fade-in-left"
+        data-wow-delay="600ms"
+      >
+        <span>Notre Vision</span>
+      </h6>
+      <div
+        class="about-us__content wow fade-in-bottom"
+        data-wow-delay="600ms"
+      >
+        <p>
+          Nous croyons que le branding n’est ni esthétique ni décoratif.
+          C’est un outil stratégique qui permet à une organisation :
+          d’être comprise rapidement,
+          d’inspirer confiance,
+          de créer une expérience cohérente,
+          et de générer des résultats mesurables.
+          Une marque claire simplifie la décision d’achat.
+          Une marque floue rend toute communication coûteuse et inefficace.
+          La visibilité sans clarté devient une dépense.
+          La clarté transforme chaque action en investissement.
+        </p>
 
-                  <a href="blog-details" class="btn-black"
-                    >More About Us<img
-                      src="assets/img/icon/star-white.png"
-                      alt="image"
-                 ></a>
-                </div>
-                <span
-                  class="about-us__date wow fade-in-right"
-                  data-wow-delay="600ms"
-                  >(Est. in 2019)</span
-                >
-              </div>
-            </div>
-          </section>
+        <a href="blog-details" class="btn-black">
+          Demander un accompagnement stratégique
+          <img
+            src="assets/img/icon/star-white.png"
+            alt="image"
+         >
+        </a>
+      </div>
+
+      <span
+        class="about-us__date wow fade-in-right"
+        data-wow-delay="600ms"
+      >(Smart Vision)</span>
+    </div>
+  </div>
+</section>
 
           <!-- counter area start  -->
-          <section class="counter theme-bg-gray section-space">
-            <div class="container">
-              <div class="row mb-minus-30">
-                <div class="col-lg-4 col-sm-6">
-                  <div
-                    class="counter__item wow fade-in-left"
-                    data-wow-delay="600ms"
-                  >
-                    <h4><span class="odometer" data-count="2012">0</span></h4>
-                    <p>Founding Year</p>
-                  </div>
-                </div>
-                <div class="col-lg-4 col-sm-6">
-                  <div
-                    class="counter__item wow fade-in-bottom"
-                    data-wow-delay="600ms"
-                  >
-                    <h4><span class="odometer" data-count="100">0</span>+</h4>
-                    <p>Completed Projects</p>
-                  </div>
-                </div>
-                <div class="col-lg-4 col-sm-6">
-                  <div
-                    class="counter__item wow fade-in-right"
-                    data-wow-delay="600ms"
-                  >
-                    <h4><span class="odometer" data-count="15">0</span>+</h4>
-                    <p>Years of Experience</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </section>
+         <section class="counter theme-bg-gray section-space">
+  <div class="container">
+    <div class="row mb-minus-30">
+      <div class="col-lg-4 col-sm-6">
+        <div
+          class="counter__item wow fade-in-left"
+          data-wow-delay="600ms"
+        >
+          <h4><span class="odometer" data-count="2012">0</span></h4>
+          <p>Année de création</p>
+        </div>
+      </div>
+
+      <div class="col-lg-4 col-sm-6">
+        <div
+          class="counter__item wow fade-in-bottom"
+          data-wow-delay="600ms"
+        >
+          <h4><span class="odometer" data-count="100">0</span>+</h4>
+          <p>Projets réalisés</p>
+        </div>
+      </div>
+
+      <div class="col-lg-4 col-sm-6">
+        <div
+          class="counter__item wow fade-in-right"
+          data-wow-delay="600ms"
+        >
+          <h4><span class="odometer" data-count="15">0</span>+</h4>
+          <p>Années d’expérience</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
           <!-- counter area end  -->
 
           <!-- experience area start  -->
           <section id="about" class="experience">
             <div class="experience__media">
-              <img src="assets/img/services/experience.jpg" alt="image">
+              <img src="{{ asset('assets/img/1.jpg') }}" alt="image">
 
               <div class="experience__card">
                 <div class="experience__card-wrapper">
@@ -222,11 +236,11 @@
                     <div class="title-slider__item">
                       <h3 class="title-slider__title">
                         Webflow Design<img
-                          src="assets/img/services/services-2.jpg"
+                          src="assets/img/Design.png"
                           alt="image"
                        >
                         App design<img
-                          src="assets/img/services/services-3.jpg"
+                          src="assets/img/creative.png"
                           alt="image"
                        >Creative thinking
                       </h3>
--- resources/views/layouts/partials/navbar.blade.php
+++ resources/views/layouts/partials/navbar.blade.php
@@ -1,153 +1,172 @@
 <div class="loader-wrap">
-      <svg viewBox="0 0 1000 1000" preserveAspectRatio="none">
-        <path id="svg" d="M0,1005S175,995,500,995s500,5,500,5V0H0Z"></path>
-      </svg>
+  <svg viewBox="0 0 1000 1000" preserveAspectRatio="none">
+    <path id="svg" d="M0,1005S175,995,500,995s500,5,500,5V0H0Z"></path>
+  </svg>
 
-      <div class="loader-wrap-heading">
-        <div class="load-text">
-          <span>C</span>
-          <span>r</span>
-          <span>u</span>
-          <span>v</span>
+  <div class="loader-wrap-heading">
+    <div class="load-text">
+      <span>C</span>
+      <span>r</span>
+      <span>u</span>
+      <span>v</span>
+    </div>
+  </div>
+</div>
+
+<!-- Retour en haut -->
+<div class="progress-wrap">
+  <svg
+    class="progress-circle svg-content"
+    width="100%"
+    height="100%"
+    viewBox="-1 -1 102 102"
+  >
+    <path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98"></path>
+  </svg>
+</div>
+
+<!-- Début menu latéral -->
+<aside class="fix">
+  <div class="side-info">
+    <div class="side-info-content">
+      <div class="offset-widget offset-header">
+        <div class="offset-logo">
+          <a href="index.html">
+            <img
+              src="{{ asset('assets/img/logo/Smartvision logotype blanc.png') }}"
+              class="normal-logo"
+              alt="Logo du site"
+           >
+          </a>
+        </div>
+        <button id="side-info-close" class="side-info-close">x</button>
+      </div>
+
+      <div class="mobile-menu d-xl-none fix"></div>
+      <div class="offset-button"></div>
+
+      <div class="offset-widget-box">
+        <h2 class="title">Contactez-nous</h2>
+        <div class="contact-meta">
+          <div class="contact-item">
+            <span class="icon">
+              <i class="fa-solid fa-location-dot"></i>
+            </span>
+            <span class="text">3891 Ranchview Dr. Richardson</span>
+          </div>
+
+          <div class="contact-item">
+            <span class="icon">
+              <i class="fa-solid fa-envelope"></i>
+            </span>
+            <span class="text">
+              <a href="mailto:hello@cruvagency.com">
+                hello@cruvagency.com
+              </a>
+            </span>
+          </div>
+
+          <div class="contact-item">
+            <span class="icon">
+              <i class="fa-solid fa-phone"></i>
+            </span>
+            <span class="text">
+              <a href="tel:(505)555-0125">(505) 555-0125</a>
+            </span>
+          </div>
         </div>
       </div>
     </div>
+  </div>
+</aside>
 
-    <!-- Sroll to top -->
-    <div class="progress-wrap">
-      <svg
-        class="progress-circle svg-content"
-        width="100%"
-        height="100%"
-        viewBox="-1 -1 102 102"
-      >
-        <path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98"></path>
-      </svg>
-    </div>
-    <!-- side toggle start -->
-    <aside class="fix">
-      <div class="side-info">
-        <div class="side-info-content">
-          <div class="offset-widget offset-header">
-            <div class="offset-logo">
-              <a href="index.html">
-                <img
-                  src="{{ asset('assets/img/logo/Smartvision logotype blanc.png') }}"
-                  class="normal-logo"
-                  alt="Site Logo"
-               >
-              </a>
-            </div>
-            <button id="side-info-close" class="side-info-close">x</button>
-          </div>
-          <div class="mobile-menu d-xl-none fix"></div>
-          <div class="offset-button"></div>
-          <div class="offset-widget-box">
-            <h2 class="title">Contact US</h2>
-            <div class="contact-meta">
-              <div class="contact-item">
-                <span class="icon"
-                  ><i class="fa-solid fa-location-dot"></i
-                ></span>
-                <span class="text">3891 Ranchview Dr. Richardson</span>
-              </div>
-              <div class="contact-item">
-                <span class="icon"><i class="fa-solid fa-envelope"></i></span>
-                <span class="text"
-                  ><a href="mailto:hello@cruvagency.com"
-                    >hello@cruvagency.com</a
-                  ></span
-                >
-              </div>
-              <div class="contact-item">
-                <span class="icon"><i class="fa-solid fa-phone"></i></span>
-                <span class="text"
-                  ><a href="tel:(505)555-0125">(505) 555-0125</a></span
-                >
-              </div>
-            </div>
-          </div>
+<div class="offcanvas-overlay"></div>
+<!-- Fin menu latéral -->
+
+<!-- Début Header -->
+<header class="header-area">
+  <div class="header-main">
+    <div class="container">
+      <div class="header-area__inner">
+        <div class="header__logo">
+          <a href="index.html">
+            <img
+              src="{{ asset('assets/img/logo/Smartvision logotype rouge.png') }}"
+              class="normal-logo"
+              width="180"
+              alt="Logo du site"
+           >
+          </a>
         </div>
-      </div>
-    </aside>
-    <div class="offcanvas-overlay"></div>
-    <!-- side toggle end -->
 
-    <!-- Header area start -->
-    <header class="header-area">
-      <div class="header-main">
-        <div class="container">
-          <div class="header-area__inner">
-            <div class="header__logo">
-              <a href="index.html">
-                <img
-                  src="{{ asset('assets/img/logo/Smartvision logotype rouge.png') }}"
-                  class="normal-logo" width="180"
-                  alt="Site Logo"
-               >
-              </a>
-            </div>
-            <div class="header__nav">
-              <nav class="main-menu">
-                <ul>
-                  <li class="menu-item-has-children">
-                    <a href="#">Home</a>
-                    <ul class="dp-menu">
-                      <li><a href="index.html">Home</a></li>
-                      <li><a href="index-2.html">Home-2</a></li>
-                    </ul>
-                  </li>
-                  <li><a href="about.html">About</a></li>
-                  <li class="menu-item-has-children">
-                    <a href="#">Services</a>
-                    <ul class="dp-menu">
-                      <li><a href="services.html">Services</a></li>
-                      <li>
-                        <a href="services-details.html">Service Details</a>
-                      </li>
-                    </ul>
-                  </li>
-
-                  <li class="menu-item-has-children">
-                    <a href="#">Portfolio</a>
-                    <ul class="dp-menu">
-                      <li><a href="portfolio-grid.html">Portfolio Grid</a></li>
-                      <li><a href="portfolio-list.html">Portfolio List</a></li>
-                      <li>
-                        <a href="portfolio-details.html">Portfolio Details</a>
-                      </li>
-                    </ul>
-                  </li>
-                  <li class="menu-item-has-children">
-                    <a href="#">Pages</a>
-                    <ul class="dp-menu">
-                      <li><a href="team.html">Team Page</a></li>
-                      <li><a href="pricing.html">Pricing Page</a></li>
-                      <li><a href="faq.html">Faq</a></li>
-                      <li><a href="404.html">404 Page</a></li>
-                    </ul>
-                  </li>
-                  <li class="menu-item-has-children">
-                    <a href="#">Blog</a>
-                    <ul class="dp-menu">
-                      <li><a href="blog-grid.html">Blog grid</a></li>
-                      <li><a href="blog-list.html">Blog list</a></li>
-                      <li><a href="blog-details.html">Blog Details</a></li>
-                    </ul>
-                  </li>
-                  <li><a href="contact-us.html">Contact</a></li>
+        <div class="header__nav">
+          <nav class="main-menu">
+            <ul>
+              <li class="menu-item-has-children">
+                <a href="#">Accueil</a>
+                <ul class="dp-menu">
+                  <li><a href="index.html">Accueil</a></li>
+                  <li><a href="index-2.html">Accueil 2</a></li>
                 </ul>
-              </nav>
-              <div class="header__navicon">
-                <button class="side-toggle">
-                  <span class="bar-icon">
-                    <span></span>
-                    <span></span>
-                  </span>
-                </button>
-              </div>
-            </div>
+              </li>
+
+              <li><a href="about.html">À propos</a></li>
+
+              <li class="menu-item-has-children">
+                <a href="#">Services</a>
+                <ul class="dp-menu">
+                  <li><a href="services.html">Services</a></li>
+                  <li>
+                    <a href="services-details.html">Détails du service</a>
+                  </li>
+                </ul>
+              </li>
+
+              <li class="menu-item-has-children">
+                <a href="#">Portfolio</a>
+                <ul class="dp-menu">
+                  <li><a href="portfolio-grid.html">Grille portfolio</a></li>
+                  <li><a href="portfolio-list.html">Liste portfolio</a></li>
+                  <li>
+                    <a href="portfolio-details.html">Détails du portfolio</a>
+                  </li>
+                </ul>
+              </li>
+
+              <li class="menu-item-has-children">
+                <a href="#">Pages</a>
+                <ul class="dp-menu">
+                  <li><a href="team.html">Équipe</a></li>
+                  <li><a href="pricing.html">Tarifs</a></li>
+                  <li><a href="faq.html">FAQ</a></li>
+                  <li><a href="404.html">Page 404</a></li>
+                </ul>
+              </li>
+
+              <li class="menu-item-has-children">
+                <a href="#">Blog</a>
+                <ul class="dp-menu">
+                  <li><a href="blog-grid.html">Blog grille</a></li>
+                  <li><a href="blog-list.html">Blog liste</a></li>
+                  <li><a href="blog-details.html">Détails du blog</a></li>
+                </ul>
+              </li>
+
+              <li><a href="contact-us.html">Contact</a></li>
+            </ul>
+          </nav>
+
+          <div class="header__navicon">
+            <button class="side-toggle">
+              <span class="bar-icon">
+                <span></span>
+                <span></span>
+              </span>
+            </button>
           </div>
         </div>
+
       </div>
-    </header>
+    </div>
+  </div>
+</header>
